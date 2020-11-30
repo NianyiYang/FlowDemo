628fe82bcdcfbf534ea474afb2cb781d804f1ece
Nianyi.Yang <>
2020-11-30 14:45:55 +0800

    update code

--- app/src/main/java/org/yang/flowdemo/CodeString.kt
+++ app/src/main/java/org/yang/flowdemo/CodeString.kt
@@ -1,7 +1,7 @@
 package org.yang.flowdemo
 
 object CodeString {
-    const val hello = """
+    const val start = """
 Log.e(tag, "当前线程：主线程")
 val job = GlobalScope.launch {
     Log.e(tag, "当前线程：线程id：$ {Thread.currentThread().id}")
@@ -18,6 +18,108 @@ job?.let {
         job?.cancel()
         Log.e(tag, "协程取消")
     }
+}
+    """
+
+    const val dispatchers = """
+GlobalScope.launch(Dispatchers.IO) {
+    Log.e(tag, "调度器从 IO 线程启动")
+    Log.e(tag, isMainThread())
+}
+    """
+
+    const val withContext = """
+GlobalScope.launch(Dispatchers.Main) {
+    withContext(Dispatchers.IO) {
+        Log.e(tag, "切换到 IO 线程")
+        Log.e(tag, isMainThread())
+    }
+    Log.e(tag, "withContext 执行完成后自动切换成主线程")
+    Log.e(tag, isMainThread())
+}
+    """
+
+    const val flowOn = """
+GlobalScope.launch(Dispatchers.Main) {
+    Log.e(tag, "使用flowOn切换线程")
+    flowOf(1)
+        .onEach {
+            Log.e(tag, "onEach.." + isMainThread())
+            delay(1000)
+        }
+        .flowOn(Dispatchers.IO)
+        .map {
+            Log.e(tag, "map.." + isMainThread())
+            it
+        }
+        .flowOn(Dispatchers.Main)
+        .collect {
+            Log.e(tag, "collect.." + isMainThread())
+        }
+}
+    """
+
+    const val error = """
+GlobalScope.launch {
+    flow {
+        emit(1)
+        throw RuntimeException()
+    }.catch {
+        Log.e(tag, "捕获异常")
+    }.onCompletion { cause ->
+        if (cause == null) {
+            Log.e(tag, "异常透明化处理，不会影响下游")
+        }
+    }.collect {
+        Log.e(tag, "执行结束")
+    }
+}
+    """
+
+    const val backPressure = """
+GlobalScope.launch {
+    testBackPressureFlow()
+        .buffer()
+//      .conflate()
+        .collect {
+//      .collectLatest {
+            delay(100)
+            Log.e(tag, "$ it 已被消费")
+        }
+}
+    """
+
+    const val operator = """
+fun onOperator(view: View) {
+    GlobalScope.launch {
+        mockNetWork1()
+            .zip(mockNetWork2()) { a, b ->
+                a + b
+            }
+            .collect {
+                Log.e(tag, "a+b = $ it")
+            }
+    }
+}
+
+suspend fun mockNetWork1() = flow {
+    delay(2000)
+    emit(1)
+}
+
+suspend fun mockNetWork2() = flow {
+    delay(1000)
+    emit(1)
+}
+    """
+
+    const val md = """
+GlobalScope.launch {
+    val sum = (1..5).asFlow()
+        .map { it * it }
+        .reduce { a, b -> a + b }
+    // 1 + 4 + 9 + 16 + 25
+    Log.e(tag,"平方和 = $ sum")
 }
     """
 }
--- app/src/main/java/org/yang/flowdemo/MainActivity.kt
+++ app/src/main/java/org/yang/flowdemo/MainActivity.kt
@@ -1,14 +1,15 @@
 package org.yang.flowdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 
+@ExperimentalCoroutinesApi
 class MainActivity : AppCompatActivity() {
 
     private val tag = MainActivity::class.java.simpleName
@@ -18,14 +19,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tv_start.showCode(CodeString.hello)
+        tv_start.showCode(CodeString.start)
+        tv_dispatchers.showCode(CodeString.dispatchers)
+        tv_with_context.showCode(CodeString.withContext)
+        tv_flowOn.showCode(CodeString.flowOn)
+        tv_error.showCode(CodeString.error)
+        tv_back_pressure.showCode(CodeString.backPressure)
+        tv_operator.showCode(CodeString.operator)
+        tv_md.showCode(CodeString.md)
     }
 
     fun startOnGlobalScope(view: View) {
         Log.e(tag, "当前线程：主线程")
         job = GlobalScope.launch {
-            Log.e(tag, "当前线程：线程id：${Thread.currentThread().id}")
-            delay(5000)
+            Log.e(tag, isMainThread())
+            delay(3000)
             Log.e(tag, "协程执行结束")
         }
         Log.e(tag, "主线程执行结束")
@@ -41,19 +49,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onDispatchers(view: View) {
+//        lifecycleScope.launch {
+//
+//        }
         GlobalScope.launch(Dispatchers.IO) {
-            Log.e(tag, "当前线程：线程id：${Thread.currentThread().id}")
-            Log.e(tag, "当前是否是主线程：${Thread.currentThread() == Looper.getMainLooper().thread}")
+            Log.e(tag, "调度器从 IO 线程启动")
+            Log.e(tag, isMainThread())
         }
     }
 
     fun onWithContext(view: View) {
         GlobalScope.launch(Dispatchers.Main) {
             withContext(Dispatchers.IO) {
-                Log.e(tag, "当前线程：IO 线程id：${Thread.currentThread().id}")
+                Log.e(tag, "切换到 IO 线程")
+                Log.e(tag, isMainThread())
             }
             Log.e(tag, "withContext 执行完成后自动切换成主线程")
-            Log.e(tag, "当前是否是主线程：${Thread.currentThread() == Looper.getMainLooper().thread}")
+            Log.e(tag, isMainThread())
         }
     }
 
@@ -62,29 +74,110 @@ class MainActivity : AppCompatActivity() {
             Log.e(tag, "使用flowOn切换线程")
             flowOf(1)
                 .onEach {
-                    Log.e(tag, "0. 当前是否是主线程：${Thread.currentThread() == Looper.getMainLooper().thread}")
+                    Log.e(tag, "onEach.." + isMainThread())
                     delay(1000)
                 }
                 .flowOn(Dispatchers.IO)
                 .map {
-                    Log.e(tag, "1. 当前是否是主线程：${Thread.currentThread() == Looper.getMainLooper().thread}")
+                    Log.e(tag, "map.." + isMainThread())
                     it
                 }
                 .flowOn(Dispatchers.Main)
-                .map {
-                    Log.e(tag, "2. 当前是否是主线程：${Thread.currentThread() == Looper.getMainLooper().thread}")
-                    it
-                }
                 .collect {
-                    Log.e(tag, "3. 当前是否是主线程：${Thread.currentThread() == Looper.getMainLooper().thread}")
+                    Log.e(tag, "collect.." + isMainThread())
                 }
         }
     }
 
     fun onError(view: View) {
-
+        GlobalScope.launch {
+            flow {
+                emit(1)
+                throw RuntimeException()
+            }.catch {
+                Log.e(tag, "捕获异常")
+            }.onCompletion { cause ->
+                if (cause == null) {
+                    Log.e(tag, "异常透明化处理，不会影响下游")
+                }
+            }.collect {
+                Log.e(tag, "执行结束")
+            }
+        }
     }
 
-    fun onBackPressure(view: View) {}
-    fun onMd(view: View) {}
+    fun onBackPressure(view: View) {
+        GlobalScope.launch {
+            testBackPressureFlow()
+                .buffer()
+                .collect {
+                    delay(100)
+                    Log.e(tag, "$it 已被消费")
+                }
+        }
+    }
+
+    fun onBackPressure2(view: View) {
+        GlobalScope.launch {
+            testBackPressureFlow()
+                .conflate()
+                .collect {
+//                .collectLatest {
+                    delay(100)
+                    Log.e(tag, "$it 已被消费")
+                }
+        }
+    }
+
+    fun onBackPressure3(view: View) {
+        GlobalScope.launch {
+            testBackPressureFlow()
+                .collectLatest {
+                    delay(100)
+                    Log.e(tag, "$it 已被消费")
+                }
+        }
+    }
+
+    fun onOperator(view: View) {
+        GlobalScope.launch {
+            mockNetWork1()
+                .zip(mockNetWork2()) { a, b ->
+                    a + b
+                }
+                .collect {
+                    Log.e(tag, "a+b = $it")
+                }
+        }
+    }
+
+    private suspend fun mockNetWork1() = flow {
+        delay(2000)
+        emit(1)
+    }
+
+    private suspend fun mockNetWork2() = flow {
+        delay(1000)
+        emit(9)
+    }
+
+    @ExperimentalCoroutinesApi
+    fun onMd(view: View) {
+        GlobalScope.launch {
+            val sum = (1..5).asFlow()
+                .map { it * it }
+                .reduce { a, b -> a + b }
+            // 1 + 4 + 9 + 16 + 25
+            Log.e(tag, "平方和 = $sum")
+        }
+    }
+
+    private fun testBackPressureFlow(): Flow<Int> = flow {
+        for (i in 1..100) {
+            emit(i)
+        }
+    }
+
+    private fun isMainThread() =
+        "当前是否是主线程：${Thread.currentThread() == Looper.getMainLooper().thread}"
 }
